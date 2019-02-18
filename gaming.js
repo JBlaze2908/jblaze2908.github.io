70ff091fea69482e85ffd28f49f040d588468ad0
JBlaze2908 <JBlaze2908@users.noreply.github.com>
2019-02-18 17:25:34 +0530

    Blog

---
+++ gaming.js
@@ -0,0 +1,5 @@
+alert("Welcome to the best gaming blog on the internet.");
+var btn=document.getElementById("btn");
+btn.addEventListener("click", function(event){
+	alert("You are subscribed to our Newsletter :)")
+});
